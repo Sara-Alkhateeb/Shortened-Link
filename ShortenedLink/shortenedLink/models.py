7b734fa535e4f1460c3835e5c8782e2ddac026b0
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-11-02 10:09:49 +0300

    stage2 done

--- ShortenedLink/shortenedLink/models.py
+++ ShortenedLink/shortenedLink/models.py
@@ -22,10 +22,3 @@ class ShortenedLink(models.Model):
 
     def __str__(self):
         return self.short_link
-
-class LinkHit(models.Model):
-    shortened_link = models.ForeignKey(ShortenedLink, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.shortened_link.short_link} - {self.created_at}"
